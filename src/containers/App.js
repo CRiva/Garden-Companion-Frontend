39044447bc55f7ef9ff83c93cea2b6894739b413
Connor Riva <connor.riva@appfolio.com>
2022-07-04 16:32:10 -0400

    Update App.js to use `className` rather than `class`
    This was a copy/paste error from the bootstrap website - intended for html
    rather than JSX

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,18 +4,18 @@ import { Link, Outlet } from "react-router-dom";
 const App = () => {
   return (
     <div>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-secondary mb-3">
+        <div className="container-fluid">
           <Link className='navbar-brand' to="/">Gardener's Companion</Link>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
                 <Link className='nav-link' to="/">Home</Link>
               </li>
-              <li class="nav-item">
+              <li className="nav-item">
                 <Link className='nav-link' to="/new_plant">New Plant</Link>
               </li>
             </ul>
